804d73e4727dc151edab59c77dee18fd50585401
Johnny Ma <johnnyma1952@gmail.com>
2021-03-22 19:05:12 -0700

    client able to display poltician

--- client/pol/pol.js
+++ client/pol/pol.js
@@ -3,8 +3,9 @@ var politicianID = undefined;
 //keep all the fetchs in service for organization
 var Service = {
   origin: "http://localhost:3000/pol",
-  getPolData: function(){
-    return fetch(this.origin + '/' + politicianID + '/' + 'info').then(
+  //returns an array of the politician data for politician with politicianID: polID
+  getPolData: function(polID){
+    return fetch(this.origin + '/' + polID + '/' + 'info').then(
       (response) => {
         if(response.status === 200)
           return Promise.resolve(response.json());
@@ -21,9 +22,32 @@ var Service = {
   }
 };
 
-
+/*
+Change the display info of the politician page
+*/
+function displayPolInfo(polID){
+  Service.getPolData(polID).then((result) => {
+    let polParty = result[1];
+    let polFName = result[2];
+    let polLName = result[3];
+    let polRating = result[4];
+    document.getElementById('polName').innerHTML = polFName + ` ` + polLName;
+    document.getElementById('polParty').innerHTML = polParty;
+    document.getElementById('polRating').innerHTML = polRating;
+  }).catch((err)=>{
+    document.getElementById('polName').innerHTML = `Can't get politician with ID: ` + politicianID;
+    console.log(err);
+  });
+}
 
 var main = function(){
+  //get politicianID from the url
+  let loc = window.location.pathname;
+  let urlArr = loc.split(/\//)
+  politicianID = urlArr[2];
+
+  //update display
+  displayPolInfo(politicianID);
 }
 
 //run main function after page loads
